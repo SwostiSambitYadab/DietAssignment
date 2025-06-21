380a2507e1080cc67ba50b4fcb98976843558d7f
Rahul Kumar <rahulsinghrps9211@gmail.com>
2025-06-21 07:17:38 +0530

    Initial Commit

---
+++ Diet Assignment/Diet_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diet_AssignmentApp.swift
+//  Diet Assignment
+//
+//  Created by Rahul Kiumar on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Diet_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
